e695f8264663dfeb9d62a94fe3386282cd3a5c62
Atul Singh <asatulsingh20@gmail.com>
2022-07-17 19:49:05 +0530

    Nextjs configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const { config } = require("process");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
